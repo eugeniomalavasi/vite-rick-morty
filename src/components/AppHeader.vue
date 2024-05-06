38f52723ccea63a67cc08c0edd58f635a388219a
eugenio.malavasi <eugenio.malavasi@gmail.com>
2024-05-06 14:38:09 +0200

    feat: project folders and components created

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        test header
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        test card container
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
